a1c1a1d8847c74cb1cf9ee82fc1d54af1686f503
Ryan Lopopolo <rjl@hyperbo.la>
2019-11-10 11:43:18 -0800

    debug

--- artichoke-backend/scripts/auto_import/get_package_files.rb
+++ artichoke-backend/scripts/auto_import/get_package_files.rb
@@ -4,8 +4,11 @@
 # require a library and figure out what constants were added.
 BASE = ARGV[0]
 PACKAGE = ARGV[1]
+puts BASE
 $LOAD_PATH.unshift(BASE)
 require PACKAGE
+puts $LOADED_FEATURES
+raise 'abort'
 lib_sources = $LOADED_FEATURES.select { |f| f.include?(BASE) }
 package_sources = lib_sources.select { |f| f =~ /#{PACKAGE}/ }
 puts package_sources.sort
