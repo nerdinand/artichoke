0f9ecce6a8877907f42555ee344f3c05162ab8a0
Ryan Lopopolo <rjl@hyperbo.la>
2019-11-10 09:02:45 -0800

    fixup fs converters on windows

--- artichoke-backend/src/fs/mod.rs
+++ artichoke-backend/src/fs/mod.rs
@@ -146,7 +146,7 @@ pub fn osstr_to_bytes<'a>(
 ) -> Result<&'a [u8], Box<dyn RubyException>> {
     use crate::extn::core::exception::Fatal;
 
-    if let Ok(converted) = value.as_ref().to_str() {
+    if let Some(converted) = value.to_str() {
         Ok(converted.as_bytes())
     } else {
         Err(Box::new(Fatal::new(
@@ -176,7 +176,7 @@ pub fn bytes_to_osstr<'a>(
     use crate::extn::core::exception::Fatal;
 
     if let Ok(converted) = str::from_utf8(value) {
-        Ok(OsStr::from(converted))
+        Ok(OsStr::new(converted))
     } else {
         Err(Box::new(Fatal::new(
             interp,
