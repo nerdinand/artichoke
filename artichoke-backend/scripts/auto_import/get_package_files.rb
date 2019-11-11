c9a946536fc9ff2ff6e8378fec3fb263801e579b
Ryan Lopopolo <rjl@hyperbo.la>
2019-11-10 18:02:48 -0800

    Handle both path separators during build on windows

--- artichoke-backend/scripts/auto_import/get_package_files.rb
+++ artichoke-backend/scripts/auto_import/get_package_files.rb
@@ -4,9 +4,13 @@
 # require a library and figure out what constants were added.
 BASE = ARGV[0]
 PACKAGE = ARGV[1]
+
 $LOAD_PATH.unshift(BASE)
+
 require PACKAGE
-raise "abort: base  = #{BASE}\nloaded features = #{$LOADED_FEATURES.inspect}"
+
 lib_sources = $LOADED_FEATURES.select { |f| f.include?(BASE) }
+lib_sources += $LOADED_FEATURES.select { |f| f.include?(BASE.gsub('\\', '/')) }
 package_sources = lib_sources.select { |f| f =~ /#{PACKAGE}/ }
+
 puts package_sources.sort
